b6b3afd6f3ee689f19a22bfe2e1023c81b8b1b4c
ervin <ervin@ervin.myguest.virtualbox.org>
2023-04-09 18:38:27 +0800

    changes

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -57,7 +57,7 @@ def generate_launch_description():
 
     diff_drive_spawner = Node(
         package="controller_manager",
-        executable="spawner",
+        executable="spawner.py",
         arguments=["diff_cont"],
     )
 
@@ -70,7 +70,7 @@ def generate_launch_description():
 
     joint_broad_spawner = Node(
         package="controller_manager",
-        executable="spawner",
+        executable="spawner.py",
         arguments=["joint_broad"],
     )
 

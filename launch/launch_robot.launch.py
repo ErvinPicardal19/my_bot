cf517719d081d92b491309b06633c47ff26b9a54
dev <dev@dev.myguest.virtualbox.org>
2023-04-11 00:22:52 +0800

    fix

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -28,11 +28,11 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
-    joystick = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','joystick.launch.py'
-                )]), launch_arguments={'use_sim_time': 'false'}.items()
-    )
+    # joystick = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory(package_name),'launch','joystick.launch.py'
+    #             )]), launch_arguments={'use_sim_time': 'false'}.items()
+    # )
 
     twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
     twist_mux_node = Node(
@@ -102,7 +102,7 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
-        joystick,
+        # joystick,
         twist_mux_node,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
